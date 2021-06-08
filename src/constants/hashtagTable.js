40f638583aea5b5625140b03063ca971a199cdf6
jackmqiu <jackqiu2016@gmail.com>
2021-06-08 15:22:51 -0700

    add hashtag table constant

---
+++ src/constants/hashtagTable.js
@@ -0,0 +1,23 @@
+const hashtagTable = {
+  airsoft: {
+    airsoft: true,
+    airsoftloadouts: true,
+    speedqb: false,
+    milsim: false,
+  },
+  guns: {
+    tacticalgear: false,
+  },
+  vlogging: {
+    vlogging: true,
+    selfie: false,
+  },
+  cars: {
+    cars: true,
+    tuning: true,
+    import: false,
+    drifting: false,
+  },
+};
+
+export { hashtagTable };
