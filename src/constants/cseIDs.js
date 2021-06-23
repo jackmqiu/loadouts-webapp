df597a1f2dcb23f902967a2ff77403697ecccc4f
jackmqiu <jackqiu2016@gmail.com>
2021-06-23 11:03:40 -0700

    remove skincare vlogging crafts for pc onebag camping

--- src/constants/cseIDs.js
+++ src/constants/cseIDs.js
@@ -2,11 +2,14 @@ const cseIDs = {
   airsoft: '286ad10865fb2879c',
   www: '286ad10865fb2879c',
   loadouts: '286ad10865fb2879c',
-  vlogging: '8e12ffab12a80e3c2',
-  crafts: '0639ef9d42a24325d',
-  skin: '4f5e872aad5b9c62a',
   guns: 'c35f3f1fdcb04e91c',
   cars: '8c916d032552115a5',
+  camping: '9b637d6c19e98f4c8',
+  pc: 'e8f83c87068f24798',
+  onebag: '4dee6389ae93abd9a',
+  skin: '4f5e872aad5b9c62a',
+  vlogging: '8e12ffab12a80e3c2',
+  crafts: '0639ef9d42a24325d',
 };
 
 export { cseIDs };
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export { hashtagTable } from './hashtagTable';
 export { cseIDs } from './cseIDs';
-export { showHome } from './homepageTable';
+export { nonCategories } from './nonCategories';
 export { mainUI } from './discoverUI';
 export { nonIDpaths } from './nonIDpaths';
 export { categoriesList } from './categoriesList';
